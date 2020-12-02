95caa39b677f8f8b517b22c6e061b009da1f14e8
Tanya He <qing.he@hec.edu>
2020-12-02 13:00:25 -0800

    cal avg reviews

--- r.py
+++ r.py
@@ -3,7 +3,14 @@ count = 0
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
-		if count % 1000 == 0:
+		count += 1
+		if count % 10000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
+print('all reviews have been read, there are', len(data), 'in total')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+	print(sum_len)
+
+print('the avg length of the reviews is', sum_len/len(data))
